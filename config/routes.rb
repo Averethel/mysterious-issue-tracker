ef4e0906c38ec3e412785453e46b73a05af71ac6
Krzysztof Sakwerda <ksakwerda@gmail.com>
2015-09-08 14:38:00 +0200

    ! root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'api/v1/issues#index'
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :issues, except: [:new, :edit] do
