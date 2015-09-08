0e19bb8a115032ecdbd2e448411f9a7c42a3fa85
Krzysztof Sakwerda <ksakwerda@gmail.com>
2015-09-09 01:57:09 +0200

    + Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.create!(username: 'admin', role: :admin, password: 'admin', password_confirmation: 'admin')
+5.times{ |i| User.create!(username: "user_#{i}", password: 'user', password_confirmation: 'user')}
+
+10.times do |i|
+  issue = Issue.create!(
+    title: "Issue ##{i}",
+    description: "Some very importatn description of issue #{i}",
+    priority: Issue.priorities.keys.sample,
+    creator: User.all.sample,
+
+  )
+
+  issue.update_attributes(status: Issue.statuses.keys.sample)
+  if i % 3 != 0
+    issue.update_attributes(assignee: User.all.sample)
+  end
+end
+
+30.times do |i|
+  Comment.create!(
+    body: '+1',
+    issue: Issue.all.sample,
+    creator: User.all.sample
+  )
+end
